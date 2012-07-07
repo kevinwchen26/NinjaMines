e10db75c08822b099b295f82477ad3b303a3accb
Kevin <kevinwchen26@gmail.com>
2012-07-07 04:28:39 -0500

    First Commit

---
+++ src/com/NinjaMines/MainActivity.java
@@ -0,0 +1,10 @@
+package com.NinjaMines;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
